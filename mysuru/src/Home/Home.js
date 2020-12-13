ce95a26f54152bef28b65ef52dd00f685d382816
Sriram2001 <sriramudupi19@gmail.com>
2020-12-13 11:21:22 +0530

    Added animation

--- mysuru/src/Home/Home.js
+++ mysuru/src/Home/Home.js
@@ -1,6 +1,7 @@
 import './Home.scss';
 import Nav from '../Nav/Nav';
 import { Link } from 'react-router-dom';
+import Fade from 'react-reveal/Fade';
 
 export default function Home() {
     return (
@@ -8,21 +9,30 @@ export default function Home() {
             <Nav sticky="false" transp="true" />
             <section className="home">
                 <div style={{ fontFamily: "'Playfair Display', serif" }}>
-                    <h1>Mysuru</h1>
-                    <i>Cultural capital of Karnataka</i>
+                    <Fade bottom cascade>
+                        <h1>Mysuru</h1>
+                        <p>Cultural capital of Karnataka</p>
+                    </Fade>
                 </div>
             </section>
             <section className="about">
                 <div>
-                    <p className='about-ind about-big'>
-                        Mysuru, the the princely city of palaces, people and smells is worth a visit whatever the month or season. Mysuru is a dream city that never lets down the visitors with its clean, light and easygoing environment.
-                </p><p className="about-ind">
-                        Steeped in tradition, Mysuru is located at the epicenter of Indian history, culture, and timeless elegance. The city of Mysuru, once the capital of the former mysuru state, is now the cultural and tourist center of the expanded Karnataka State.
-                </p>
-                    <p className="about-ind">
-                        Come visit Mysuru to experience the rich history and tradition awaiting you...
-                </p>
-                    <Link type="button" to="/places">Explore Places</Link>
+                    <Fade left>
+                        <p className='about-ind about-big'>
+                            Mysuru, the the princely city of palaces, people and smells is worth a visit whatever the month or season. Mysuru is a dream city that never lets down the visitors with its clean, light and easygoing environment.
+                        </p>
+                    </Fade>
+                    <Fade right>
+                        <p className="about-ind">
+                            Steeped in tradition, Mysuru is located at the epicenter of Indian history, culture, and timeless elegance. The city of Mysuru, once the capital of the former mysuru state, is now the cultural and tourist center of the expanded Karnataka State.
+                        </p>
+                        <p className="about-ind">
+                            Come visit Mysuru to experience the rich history and tradition awaiting you...
+                        </p>
+                    </Fade>
+                    <Fade bottom>
+                        <Link type="button" to="/places">Explore Places</Link>
+                    </Fade>
                 </div>
             </section>
         </>
